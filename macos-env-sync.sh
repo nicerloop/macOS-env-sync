fd8c1a03979b507fddf0e3a9606840bd37b76127
Loïc PÉRON <loic.peron@gmail.com>
2025-01-29 17:35:59 +0100

    unset removed variables

--- macos-env-sync.sh
+++ macos-env-sync.sh
@@ -31,16 +31,31 @@ if [ $# -eq 0 ]; then
     set -- "$HOME/.zshrc"
 fi
 
+PROTECTED_NAMES="SSH_AUTH_SOCK"
+SESSION_VALUES=$(launchctl print gui/"$(launchctl manageruid)" | awk '/environment = {/,/}/' | sed '1d;$d' | sed -e 's/^\t*//' -e 's/ => /=/g' | sort)
+
 NAMES=$(grep '^[:space:]*export' "$@" | cut -d '=' -f 1 | cut -d ' ' -f 2 | sort -u)
 NAME_REGEXS=$(echo "$NAMES" | sed 's/^/^/;s/$/=/')
 VALUES=$(env | grep "$NAME_REGEXS" | sort -u -t '=' -k 1)
+
 for VALUE in $VALUES; do
     NAME=$(echo "$VALUE" | cut -d '=' -f 1)
     VALUE=$(echo "$VALUE" | cut -d '=' -f 2)
     if [ "$VALUE" != "$(launchctl getenv "$NAME")" ]; then
+        echo "$NAME=$VALUE"
         launchctl setenv "$NAME" "$VALUE"
         UPDATED=true
     fi
 done
+
+for VALUE in $SESSION_VALUES; do
+    NAME=$(echo "$VALUE" | cut -d '=' -f 1)
+    if ! (echo "$NAMES" "$PROTECTED_NAMES") | grep -q -F "$NAME"; then
+        echo "unset $NAME"
+        launchctl unsetenv "$NAME"
+        UPDATED=true
+    fi
+done
+
 $UPDATED && $NOTIFY && terminal-notifier -message 'Environment variables updated'
 $WATCH && fswatch -1 "$@" >/dev/null
